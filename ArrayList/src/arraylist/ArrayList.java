3a226f1b04581b94e544142e393d5b132873f347
Dashk179 <dash_arturo@hotmail.com>
2019-11-14 23:55:04 -0600

    Twitter: @Arti179

--- ArrayList/src/arraylist/ArrayList.java
+++ ArrayList/src/arraylist/ArrayList.java
@@ -8,14 +8,54 @@ package arraylist;
 /**
  *
  * @author dash
+ *
  */
+import java.util.Arrays;
+import javax.swing.JOptionPane;
+
 public class ArrayList {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        int numeros[] = new int[5];
+        int numerotes[]= new int [numeros.length];
+        String palabras[] = new String[5];
+        int i;
+        for (i = 0; i < 5; i++) {
+            numeros[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa el elemento del indice" + i));
+
+        }
+
+        //Mostrando los datos como los ingresamos 
+        System.out.println("Los datos sin ordenar son: ");
+        for (i = 0; i < 5; i++) {
+            System.out.print("[" + numeros[i] + "]");
+        }
+
+        System.out.println("\nLos datos ordenados son :");
+        Arrays.sort(numeros);//Aqui ordenamos el arreglo numeros 
+        for (i = 0; i < 5; i++) {
+            System.out.print("[" + numeros[i] + "]");
+        }
+
+        //Rellando al arreglo palabras 
+        Arrays.fill(palabras, "Bienvenidos a la Fiesta");
+        System.out.println("\n\nElemento del arreglo palabras");
+        for (i = 0; i < 5; i++) {
+            System.out.println("[" + palabras[i] + "]");
+
+        }
+        
+        //Copiando los elementos del arreglo numeros a numerotes
+        System.arraycopy(numeros, 0, numerotes, 0, numeros.length);//Parte la posicion 0 del indice
+          System.out.println("\n\nElemento del arreglo numerotes");
+        for (i = 0; i < 5; i++) {
+            System.out.println("[" + numerotes[i] + "]");
+
+        }
     }
-    
+
 }
